50a38a03b977c203c0e5069b6e31937ed4db3be0
Georgi Spasov <georgi.spasov@outlook.com>
2017-11-08 12:34:26 +0200

    Assignment & Course Services. Changed Assignment model

--- UpdateMe/UpdateMe.Data/Models/Assignment.cs
+++ UpdateMe/UpdateMe.Data/Models/Assignment.cs
@@ -19,5 +19,9 @@ namespace UpdateMe.Data.Models
         public int CourseId { get; set; }
 
         public virtual Course Course { get; set; }
+
+        public string ApplicationUserId { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
     }
 }
