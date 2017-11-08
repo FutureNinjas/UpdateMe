c4eb0736aeba1ee9b7f8437c12e6551235199a27
georgievlb <georgiev.lb@gmail.com>
2017-11-08 15:33:17 +0200

    Admin area

---
+++ UpdateMe/UpdateMe/Areas/Admin/Models/CourseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using UpdateMe.Data.Models;
+
+namespace UpdateMe.Areas.Admin.Models
+{
+    public class CourseViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public static Expression<Func<Course, CourseViewModel>> Create
+        {
+            get
+            {
+                return c => new CourseViewModel()
+                {
+                    Id = c.Id, 
+                    Name = c.Name, 
+                    Description = c.Description
+                };
+            }
+        }
+    }
+}
---
+++ UpdateMe/UpdateMe/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq.Expressions;
+using UpdateMe.Data.Models;
+
+namespace UpdateMe.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        //static because we don't need to decouple from it - this is used for unit testing
+        public static Expression<Func<ApplicationUser, UserViewModel>> Create
+        {
+            get
+            {
+                return u => new UserViewModel()
+                {
+                    Id = u.Id,            //vzimame dve koloni ot tablicata -Id
+                    Username = u.UserName // i username
+                };
+            }
+        }
+    }
+}
