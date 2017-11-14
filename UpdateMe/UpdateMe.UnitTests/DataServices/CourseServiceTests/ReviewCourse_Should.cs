697400747d0db5230602a7619a9c51bb14cccc03
Iliyan <solommr5kk@gmail.com>
2017-11-14 14:30:34 +0200

    Button to Quiz section

---
+++ UpdateMe/UpdateMe.UnitTests/DataServices/CourseServiceTests/ReviewCourse_Should.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpdateMe.Data;
+using UpdateMe.Data.Models;
+
+namespace UpdateMe.UnitTests.DataServices.CourseServiceTests
+{
+    [TestClass]
+    public class ReviewCourse_Should
+    {
+        [TestMethod]
+
+        public void ReturnPropperDataWhenPropperIdIsPassed()
+        {
+            //Arrange
+            string name = "ValidateStringTestName";
+            string description = "ValidateStringTestDescription";
+            int id = 1;
+            int passScore = 99;
+            
+
+            var contextMock = new Mock<UpdateMeDbContext>();
+            var course = new CourseModel() { Id = id, Name = name, Description = description, PassScore = passScore };
+            var courseViewModel = new CourseViewModel() { Id = id, Name = nameMock, Description = descriptionMock, PassScore = passScoreMock };
+
+            List<Course> courses = new List<Course>() { course };
+        }
+    }
+}
