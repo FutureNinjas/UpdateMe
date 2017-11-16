73de335ae32bb961fabd592a67baf1f5210cdee6
georgievlb <georgiev.lb@gmail.com>
2017-11-16 13:27:58 +0200

    Added home controller tests

---
+++ UpdateMe/UpdateMe.UnitTests/Controllers/HomeControllerTests/HomeControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UpdateMe;
+using UpdateMe.Controllers;
+using UpdateMe.Data;
+using Moq;
+
+namespace UpdateMe.UnitTests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void About()
+        {
+            var dbContextMock = new Mock<UpdateMeDbContext>();
+            // Arrange
+            HomeController controller = new HomeController(dbContextMock.Object);
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            var dbContextMock = new Mock<UpdateMeDbContext>();
+            // Arrange
+            HomeController controller = new HomeController(dbContextMock.Object);
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ UpdateMe/UpdateMe.UnitTests/DataServices/Controllers/CoursesControllerTests/ListUserCourses_Should.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UpdateMe.UnitTests.Controllers.CoursesControllerTests
+{
+    [Ignore]
+    [TestClass]
+    public class ListUserCourses_Should
+    {
+    }
+}
